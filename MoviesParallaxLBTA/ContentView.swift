6a4f1809bd866e08b42f9b47e974230c1c4263bc
rick2785 <rjhrabowskie@gmail.com>
2021-01-18 09:59:09 -0600

    Initial Commit

---
+++ MoviesParallaxLBTA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesParallaxLBTA
+//
+//  Created by RJ Hrabowskie on 1/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesParallaxLBTA/MoviesParallaxLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesParallaxLBTAApp.swift
+//  MoviesParallaxLBTA
+//
+//  Created by RJ Hrabowskie on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesParallaxLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
